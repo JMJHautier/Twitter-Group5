51638803ab66e07cdd4b5618ce4cf11440546184
Heike <heike.kleiner30@googlemail.com>
2021-04-15 16:23:40 +0200

    Add users

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,29 @@
 const pool = require('../pg.js');
 
-
 const getAllUsers = async (req, res) => {
+// res.send('get All users')
 
-   res.send('get All users')
+   try {
+      const results = await pool.query('SELECT * FROM users;')
+      console.log(results)
+      if(!results.rowCount) return res.json({message: 'No users found'})
+      res.json(results.rows)
+   } catch (error) {
+      res.status(500).json(error)
+   }
 }
 
 const getSingleUser = async (req, res) => {
-   res.send('get single user')
+// res.send('get single user')
 
+   try {
+      const { id } = req.params;
+      const result = await pool.query('SELECT * FROM users WHERE id=$1;', [id]);
+      if (!result.rowCount) res.status(404).json({ message: 'No user found' });
+      res.json(result.rows[0]);
+   } catch (error) {
+      res.status(500).json(error);
+   }
 }
 
 module.exports = {getAllUsers, getSingleUser}
--- pg.js
+++ pg.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'gqogxscw ',
+  user: 'gqogxscw',
   host: 'rogue.db.elephantsql.com',
-  database: 'gqogxscw ',
+  database: 'gqogxscw',
   password: 'z-nYh0-oRD_uYMUPUsFmYnH92hsL9V5t',
   port: 5432
 });
 
 module.exports = pool;
+
